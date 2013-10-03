5790782374c6fc70acc83b1ec207e7dce92def46
John Zdanis <Zdanis.john@gmail.com>
2013-10-03 18:41:29 -0400

    adding some config files back

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FAMOUS::Application.config.secret_token = '068dcbc06cc074df61e93056ff1e0612661010d48ed657fa755a888293255bd74ca622e08733ed42ff346a5ae0aaf280d3fddc52e1dae643950314cf9acad87c'

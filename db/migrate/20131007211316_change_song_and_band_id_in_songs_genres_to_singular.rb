27d5d11b9ebb188a12daffb10727a7a1adaa4fbe
John Zdanis <Zdanis.john@gmail.com>
2013-10-07 18:38:38 -0400

    remove duplicate migrations

--- db/migrate/20131007211316_change_song_and_band_id_in_songs_genres_to_singular.rb
+++
@@ -1,8 +0,0 @@
-class ChangeSongAndBandIdInSongsGenresToSingular < ActiveRecord::Migration
-  def change
-  	change_table :songs_genres do |t|
-  		t.rename :songs_id, :song_id
-  		t.rename :genres_id, :genre_id
-  	end
-  end
-end

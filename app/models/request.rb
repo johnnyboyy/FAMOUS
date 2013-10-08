50dc916f4bc374a723d35ae4661d4d1436e22ee4
John Zdanis <Zdanis.john@gmail.com>
2013-10-08 19:06:59 -0400

    removed unnessecary views and finish tests and validations for request model

--- app/models/request.rb
+++ app/models/request.rb
@@ -2,5 +2,21 @@ class Request < ActiveRecord::Base
   belongs_to :user, foreign_key: :sender
   has_many :users, foreign_key: :reciever
 
+  validates_presence_of :request_type, :band_id, :status, :sender, :reciever
+  validates_numericality_of :band_id, :reciever, :sender
+  validate :status_must_be_correct, on: :create
+  validate :request_type_must_be_correct, on: :create
 
+
+  def status_must_be_correct
+    if status != 'pending'
+      errors.add(:status, "must be set to pending!")
+    end
+  end
+  def request_type_must_be_correct
+    if request_type != "member_request"
+      errors.add(:status, "must be set to pending!")
+    end
+  end
+  
 end
--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -1,5 +1,75 @@
 require 'spec_helper'
 
 describe Request do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:req) { Request.new(request_type: "member_request",
+                band_id: 1,
+                status: "pending",
+                sender: 1,
+                reciever: 2,
+                message: "test" )}
+  describe "Basic presence_of tests;" do
+    it "should accept a valid request" do
+      expect(req.save).to eq(true)
+    end
+    it "should not allow a blank sender field" do
+      req.sender = nil
+
+      expect(req.save).to eq(false)
+    end
+    it "should not allow a blank band_id field" do
+      req.band_id = nil
+
+      expect(req.save).to eq(false)
+    end
+    it "should not allow a blank status field" do
+      req.status = nil
+
+      expect(req.save).to eq(false)
+    end
+    it "should not allow a blank request_type field" do
+      req.request_type = nil
+
+      expect(req.save).to eq(false)
+    end
+    it "should not allow a blank reciever field" do
+      req.reciever = nil
+
+      expect(req.save).to eq(false)
+    end
+  end
+  describe "field specific tests;" do
+    it "reciever field should only accept a number" do
+        req.reciever = "test"
+      expect(req.save).to eq(false)
+    end
+    it "sender field should only accept a number" do
+        req.sender = "test"
+      expect(req.save).to eq(false)
+    end
+    it "band_id field should only accept a number" do
+        req.band_id = "test"
+      expect(req.save).to eq(false)
+    end
+    it "status field should not accept 'bad_status'" do
+      req.status = "bad_status"
+
+      expect(req.save).to eq(false)
+    end
+    it "status field should accept 'pending'" do
+      req.status = "pending"
+
+      expect(req.save).to eq(true)
+    end
+    it "status field should not accept 'bad_request'" do
+      req.request_type = "bad_request"
+
+      expect(req.save).to eq(false)
+    end
+    it "status field should accept 'band_request'" do
+      req.request_type = "member_request"
+
+      expect(req.save).to eq(true)
+    end
+  end
 end

d4d35b63966387b1733d174686dd9025ceefe012
John Zdanis <Zdanis.john@gmail.com>
2013-10-10 10:53:00 -0400

    make genre buttons show up on main page

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,8 +2,13 @@ class MainController < ApplicationController
 
 	def index
 		@songs = Song.all.order("fame DESC")
-		@bands = Band.all
+		@bands = Band.all.order("fame DESC")
     @topsong = Song.joins(:likes).where("likes.created_at > ?", 1.day.ago).order("songs.fame DESC").first  || Song.order("fame DESC").first
     @topsongoftheweek = Song.joins(:likes).where("likes.created_at > ?", 7.day.ago).order("songs.fame DESC").first  || Song.order("fame DESC").first
+
+    @five_or_under = Genre.count
+    @genres = Genre.all
+    @page_options = 'main/mainOptions'
   end
+
 end

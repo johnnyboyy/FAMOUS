01858494c361ec74248d9b3cbaeeb73d9a0e7094
John Zdanis <Zdanis.john@gmail.com>
2013-10-09 23:34:59 -0400

    write test to ensure artist name gets updated on all songs by that artist when artist name is changed

--- spec/models/band_spec.rb
+++ spec/models/band_spec.rb
@@ -5,5 +5,16 @@ describe Band do
   	expect(Band.new.save).to eq(false)
   end
 
+  it "should update songs artist field when bands name is changed" do
+    band = Band.new(name: "test_band")
+    band.save
+    band.songs.build(title: "test_song", artist: band.name).save(validate: false)
+
+    band.name = "different_name"
+    band.save(validate: false)
+
+    expect(band.songs.first.artist).to eq('different_name')
+  end
+
 
 end

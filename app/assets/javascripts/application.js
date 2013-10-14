653aa356de2091c6e16d7a313bc55c0a7f075955
John Zdanis <Zdanis.john@gmail.com>
2013-10-14 19:12:36 -0400

    cleaned javascript dir, made 'new song' more conveinient by auto-pop field, and added missing font for gumby

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,7 @@
 //= require jquery_ujs
 //= require jquery.ui.all
 //= require modernizr
-//= require gumby.min
-//= require bands
+//= require z_gumby.min
 //= require navbar
+//= require z_bands
+
--- app/assets/javascripts/bandMemberOptions.js
+++
@@ -1,7 +0,0 @@
-
-$(document).ready(function(){
-	$('.member_options').hide();
-	$('.member_options_toggle').on('click', function(){
-		$('.member_options').toggle();
-	});
-});
